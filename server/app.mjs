ff0a9bab5e3cbf0e5612c79f4bcfd7962f0acb22
Pattaradol <Pattaradol.k@gmail.com>
2025-09-03 14:48:30 +0700

    feat: complete assignment

--- server/app.mjs
+++ server/app.mjs
@@ -1,4 +1,5 @@
 import express from "express";
+import validateAssignment from "./middlewares/assignmentValidate.mjs"; // นำเข้า validateAssignment
 
 const app = express();
 const port = 4001;
@@ -8,8 +9,8 @@ app.use(express.json());
 app.get("/test", (req, res) => {
   return res.json("Server API is working 🚀");
 });
-
-app.post("/assignments", (req, res) => {
+  //เอา validateAssignment มาไว้ตรงนี้
+app.post("/assignments", validateAssignment, (req, res) => {
   // สมมุติว่าตรงนี้มีโค้ดที่เขียน Query เชื่อมต่อกับฐานข้อมูล
   // เพื่อสร้าง Assignment เขียนเอาไว้แล้ว
   return res.status(201).json({ message: "Create assignment successfully" });
---
+++ server/middlewares/assignmentValidate.mjs
@@ -0,0 +1,32 @@
+const validateAssignment = (req, res, next) => {
+    const { title, description, dueDate, courseId } = req.body;
+    //ตรวจสอบว่ามี field ครบ ตาม req หรือไม่
+    const errors = [];
+    //ตรวจสอบความถูกต้องของข้อมูล
+    if(!title) errors.push("Title is required");
+    if(!description) errors.push("Description is required");
+    if(!dueDate) errors.push("Due date is required");
+    if(!courseId) errors.push("Course id is required");
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (email && !emailRegex.test(email)) {
+        errors.push("Invalid email format")
+    };
+    //ตรวจสอบความถูกต้องของข้อมูล
+    const allowedCategories = ["Math", "English", "Biology"]
+    if (category && !allowedCategories.includes(category)) {
+        errors.push("category must be one of the following: Math, English, Biology")
+    };
+    //ตรวจสอบความยาวของข้อมูล
+    if (content && (content.length < 500 || content.length > 1000)) {
+        errors.push("content must be between 5 and 1000 characters")
+    }
+
+    //ส่งข้อมูลกลับ
+    if (errors.length > 0) {
+        return res.status(400).json({ errors })
+    }
+    next();
+}
+
+export default validateAssignment;
